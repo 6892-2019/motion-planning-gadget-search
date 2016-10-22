1941fc58181ddc939e32152d216d3722c0895e65
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2016-10-22 01:11:22 -0400

    Test determinize() and totalize()
    determinize() and totalize() aren't really useful on their own, but we
    have to make them public for testing.  That's probably okay because
    Automaton lives in the impl namespace, so it's already understood to not
    be part of the public API.

--- src/automaton.hpp
+++ src/automaton.hpp
@@ -217,6 +217,10 @@ public:
 		return a;
 	}
 
+	ptr clone() const {
+		return new Automaton(*this);
+	}
+
 	/**
 	 * Returns true if this automaton is known to be deterministic.
 	 */
@@ -235,10 +239,28 @@ public:
 	}
 
 	/**
-	 * Returns true if the machine accepts the given string (as symbol indices)
-	 * and false otherwise.
+	 * Runs this automaton on the given string.
+	 * @returns true iff the machine accepts the given string of symbol indices
 	 */
 	bool run(std::initializer_list<unsigned int> string) const {
+		//This overload exists because the compiler won't deduce initializer_list
+		//for the IteratorRange overload below.
+		return run(string.begin(), string.end());
+	}
+	/**
+	 * Runs this automaton on the given string.
+	 * @returns true iff the machine accepts the given string of symbol indices
+	 */
+	template<class InputIterator>
+	bool run(InputIterator begin, InputIterator end) const {
+		return run(boost::make_iterator_range(begin, end));
+	}
+	/**
+	 * Runs this automaton on the given string.
+	 * @returns true iff the machine accepts the given string of symbol indices
+	 */
+	template<class IteratorRange>
+	bool run(const IteratorRange& string) const {
 		//Breadth-first search.
 		std::unordered_set<state_type> current, next;
 		current.insert(0); //TODO: assuming 0 is the initial state
@@ -251,8 +273,86 @@ public:
 		}
 		return std::any_of(current.begin(), current.end(), [this](state_type s){return accept_[s];});
 	}
+
+	void determinize() {
+		if (deterministic()) return;
+		//We manually sort before inserting in newstate.
+		//Unfortunately there's no small_flat_set...
+		using state_set = small_vector<state_type, 4>;
+		auto hasher = [](const state_set& set) {
+			//could be std::accumulate, I guess
+			std::size_t accum = 0;
+			for (state_type t : set)
+				accum = accum * 17 + t;
+			return accum;
+		};
+
+		//0 bucket count is fine: http://stackoverflow.com/q/14179441/3614835
+		std::unordered_map<state_set, state_type, decltype(hasher)> newstate(0, hasher);
+		//pointers to keys in newstate
+		std::stack<const typename decltype(newstate)::value_type*> worklist;
+		ptr a = new Automaton;
+		a->addState();
+		auto iterSucc = newstate.insert(std::make_pair(state_set({0}), 0));
+		assert(iterSucc.second);
+		worklist.push(&*(iterSucc.first));
+
+		while (!worklist.empty()) {
+			auto current = worklist.top();
+			worklist.pop();
+
+			a->accept_[current->second] = std::any_of(current->first.begin(), current->first.end(),
+					[this](state_type state) {return this->accept_[state];});
+
+			state_set next;
+			for (symbol_type s = 0; s < AlphabetSize; ++s) {
+				next.clear();
+				for (state_type f : current->first)
+					for (state_type t : step(f, s))
+						if (std::find(next.begin(), next.end(), t) == next.end())
+							next.push_back(t);
+				if (next.empty())
+					continue; //all NFA states crashed
+				std::sort(next.begin(), next.end());
+				//TODO: is there a computeIfAbsent equivalent?
+				auto it = newstate.find(next);
+				if (it == newstate.end()) {
+					it = newstate.insert(std::make_pair(std::move(next), a->addState())).first;
+					worklist.push(&*it);
+				}
+				a->addTrans(current->second, s, it->second);
+				assert(a->deterministic());
+			}
+		}
+
+		*this = std::move(*a);
+		assert(deterministic());
+	}
+
+	/**
+	 * Fills in any missing transitions with transitions to an explicit crash
+	 * state.
+	 */
+	void totalize() {
+		state_type crash;
+		bool madeCrashState = false;
+		for (state_type s = 0; s < transitions_.size(); ++s) {
+			symbol_mask_type missing = ~outgoing(s);
+			if (missing.any()) {
+				if (!madeCrashState) {
+					crash = addState();
+					madeCrashState = true;
+				}
+				addTrans(s, missing, crash);
+			}
+		}
+	}
+
 private:
 	Automaton() : deterministic_(true) {}
+	//copy everything but the refcount
+	Automaton(const Automaton& a) : transitions_(a.transitions_), accept_(a.accept_),
+			deterministic_(a.deterministic_), refcount_(0) {}
 	//move everything but the reference counter (which is tied to the physical object)
 	Automaton& operator=(Automaton&& victim) {
 		transitions_ = std::move(victim.transitions_);
@@ -409,80 +509,6 @@ private:
 		if (!isStateDeterministic(from))
 			deterministic_ = false;
 		return true;
-	}
-
-	void determinize() {
-		if (deterministic()) return;
-		//We manually sort before inserting in newstate.
-		//Unfortunately there's no small_flat_set...
-		using state_set = small_vector<state_type, 4>;
-		auto hasher = [](const state_set& set) {
-			//could be std::accumulate, I guess
-			std::size_t accum = 0;
-			for (state_type t : set)
-				accum = accum * 17 + t;
-			return accum;
-		};
-
-		//0 bucket count is fine: http://stackoverflow.com/q/14179441/3614835
-		std::unordered_map<state_set, state_type, decltype(hasher)> newstate(0, hasher);
-		//pointers to keys in newstate
-		std::stack<const typename decltype(newstate)::value_type*> worklist;
-		ptr a = new Automaton;
-		a->addState();
-		auto iterSucc = newstate.insert(std::make_pair(state_set({0}), 0));
-		assert(iterSucc.second);
-		worklist.push(&*(iterSucc.first));
-
-		while (!worklist.empty()) {
-			auto current = worklist.top();
-			worklist.pop();
-
-			a->accept_[current->second] = std::any_of(current->first.begin(), current->first.end(),
-					[this](state_type state) {return this->accept_[state];});
-
-			state_set next;
-			for (symbol_type s = 0; s < AlphabetSize; ++s) {
-				next.clear();
-				for (state_type f : current->first)
-					for (state_type t : step(f, s))
-						if (std::find(next.begin(), next.end(), t) == next.end())
-							next.push_back(t);
-				if (next.empty())
-					continue; //all NFA states crashed
-				std::sort(next.begin(), next.end());
-				//TODO: is there a computeIfAbsent equivalent?
-				auto it = newstate.find(next);
-				if (it == newstate.end()) {
-					it = newstate.insert(std::make_pair(std::move(next), a->addState())).first;
-					worklist.push(&*it);
-				}
-				a->addTrans(current->second, s, it->second);
-				assert(a->deterministic());
-			}
-		}
-
-		*this = std::move(*a);
-		assert(deterministic());
-	}
-
-	/**
-	 * Fills in any missing transitions with transitions to an explicit crash
-	 * state.
-	 */
-	void totalize() {
-		state_type crash;
-		bool madeCrashState = false;
-		for (state_type s = 0; s < transitions_.size(); ++s) {
-			symbol_mask_type missing = ~outgoing(s);
-			if (missing.any()) {
-				if (!madeCrashState) {
-					crash = addState();
-					madeCrashState = true;
-				}
-				addTrans(s, missing, crash);
-			}
-		}
 	}
 
 	/**
--- test/automaton-test.cpp
+++ test/automaton-test.cpp
@@ -4,6 +4,82 @@
 using namespace automaton;
 using namespace automaton::impl;
 
+namespace {
+
+//for custom assertion failure messages
+//I couldn't get the compiler to find an operator<< overload, shrug
+std::string to_string(const std::vector<unsigned int>& v) {
+	std::string str;
+    str += '(';
+	auto i = v.cbegin();
+	if (i != v.cend())
+		str += std::to_string(*i);
+    for (; i != v.cend(); ++i)
+        str += std::to_string(*i);
+    str += ')';
+    return str;
+}
+
+std::vector<std::vector<unsigned int>> allStrings(unsigned int alphabetSize, unsigned int length) {
+	assert(alphabetSize > 0);
+	std::vector<std::vector<unsigned int>> ret;
+
+	//Starting from the empty string, take all strings from the last generation
+	//and append each possible symbol to them.
+	auto start = ret.size();
+	ret.emplace_back();
+	for (unsigned int n = 1; n <= length; ++n) {
+		auto end = ret.size();
+		for (auto i = start; i < end; ++i) {
+			for (unsigned int s = 0; s < alphabetSize; ++s) {
+				//We can't just ret.push_back(ret[i]) because reallocation will
+				//move ret[i] out from under us.
+				auto copy = ret[i];
+				copy.push_back(s);
+				ret.push_back(std::move(copy));
+			}
+		}
+		start = end;
+	}
+	return ret;
+}
+
+template<int AlphabetSize>
+void equivalentOnAllStrings(typename Automaton<AlphabetSize>::const_ptr a, typename Automaton<AlphabetSize>::const_ptr b, int length) {
+	for (auto& string : allStrings(AlphabetSize, length))
+		EXPECT_EQ(a->run(string), b->run(string)) << to_string(string);
+}
+
+} //end anonymous namespace
+
+TEST(AutomatonTest, AllStringsUtilFn) {
+	auto s = allStrings(2, 0);
+	EXPECT_EQ(s.size(), 1);
+	EXPECT_TRUE(s[0].empty());
+
+	s = allStrings(2, 1);
+	EXPECT_EQ(s.size(), 3);
+	auto sizeOne = [](const auto& v){return v.size() == 1;};
+	EXPECT_EQ(std::count_if(s.begin(), s.end(), sizeOne), 2);
+
+	s = allStrings(2, 2);
+	EXPECT_EQ(s.size(), 7);
+	EXPECT_EQ(std::count_if(s.begin(), s.end(), sizeOne), 2);
+	auto sizeTwo = [](const auto& v){return v.size() == 2;};
+	EXPECT_EQ(std::count_if(s.begin(), s.end(), sizeTwo), 4);
+
+	s = allStrings(2, 3);
+	EXPECT_EQ(s.size(), 15);
+	auto sizeThree = [](const auto& v){return v.size() == 3;};
+	EXPECT_EQ(std::count_if(s.begin(), s.end(), sizeThree), 8);
+}
+
+TEST(AutomatonTest, EquivOnAllStringsUtilFn) {
+	equivalentOnAllStrings<2>(Automaton<2>::empty(), Automaton<2>::empty(), 8);
+	equivalentOnAllStrings<2>(Automaton<2>::all(), Automaton<2>::all(), 8);
+	equivalentOnAllStrings<2>(Automaton<2>::lit(1), Automaton<2>::lit(1), 8);
+}
+
 TEST(AutomatonTest, EmptyLanguage) {
 	auto a = Automaton<2>::empty();
 	EXPECT_TRUE(a->deterministic());
@@ -224,4 +300,92 @@ TEST(AutomatonTest, Range) {
 	EXPECT_FALSE(s->run({0, 1}));
 	EXPECT_FALSE(s->run({0, 0, 1}));
 	EXPECT_FALSE(s->run({1, 0, 0}));
+}
+
+TEST(AutomatonTest, Clone) {
+	auto a = Automaton<2>::range(Automaton<2>::lit(0), 2, 6);
+	auto b = a->clone();
+	equivalentOnAllStrings<2>(a, b, 8);
+	auto d = Automaton<2>::cat({Automaton<2>::lit(0), Automaton<2>::lit(1)});
+	auto e = Automaton<2>::cat({Automaton<2>::lit(1), Automaton<2>::lit(0)});
+	auto f = Automaton<2>::alt({d, e});
+	auto fc = f->clone();
+	equivalentOnAllStrings<2>(f, fc, 8);
+	auto g = Automaton<2>::conj(f, d);
+	auto gc = g->clone();
+	equivalentOnAllStrings<2>(g, gc, 8);
+}
+
+TEST(AutomatonTest, Determinize) {
+	auto a = Automaton<2>::range(Automaton<2>::lit(0), 2, 6);
+	auto b = a->clone();
+	b->determinize();
+	equivalentOnAllStrings<2>(a, b, 8);
+	auto d = Automaton<2>::cat({Automaton<2>::lit(0), Automaton<2>::lit(1)});
+	auto e = Automaton<2>::cat({Automaton<2>::lit(1), Automaton<2>::lit(0)});
+	auto f = Automaton<2>::alt({d, e});
+	auto fc = f->clone();
+	fc->determinize();
+	equivalentOnAllStrings<2>(f, fc, 8);
+	auto g = Automaton<2>::conj(f, d);
+	auto gc = g->clone();
+	gc->determinize();
+	equivalentOnAllStrings<2>(g, gc, 8);
+}
+
+TEST(AutomatonTest, Totalize) {
+	auto a = Automaton<2>::range(Automaton<2>::lit(0), 2, 6);
+	auto b = a->clone();
+	b->totalize();
+	equivalentOnAllStrings<2>(a, b, 8);
+	auto d = Automaton<2>::cat({Automaton<2>::lit(0), Automaton<2>::lit(1)});
+	auto e = Automaton<2>::cat({Automaton<2>::lit(1), Automaton<2>::lit(0)});
+	auto f = Automaton<2>::alt({d, e});
+	auto fc = f->clone();
+	fc->totalize();
+	equivalentOnAllStrings<2>(f, fc, 8);
+	auto g = Automaton<2>::conj(f, d);
+	auto gc = g->clone();
+	gc->totalize();
+	equivalentOnAllStrings<2>(g, gc, 8);
+}
+
+TEST(AutomatonTest, DeterminizeTotalize) {
+	auto a = Automaton<2>::range(Automaton<2>::lit(0), 2, 6);
+	auto b = a->clone();
+	b->determinize();
+	b->totalize();
+	equivalentOnAllStrings<2>(a, b, 8);
+	auto d = Automaton<2>::cat({Automaton<2>::lit(0), Automaton<2>::lit(1)});
+	auto e = Automaton<2>::cat({Automaton<2>::lit(1), Automaton<2>::lit(0)});
+	auto f = Automaton<2>::alt({d, e});
+	auto fc = f->clone();
+	fc->determinize();
+	fc->totalize();
+	equivalentOnAllStrings<2>(f, fc, 8);
+	auto g = Automaton<2>::conj(f, d);
+	auto gc = g->clone();
+	gc->determinize();
+	gc->totalize();
+	equivalentOnAllStrings<2>(g, gc, 8);
+}
+
+TEST(AutomatonTest, TotalizeDeterminize) {
+	auto a = Automaton<2>::range(Automaton<2>::lit(0), 2, 6);
+	auto b = a->clone();
+	b->totalize();
+	b->determinize();
+	equivalentOnAllStrings<2>(a, b, 8);
+	auto d = Automaton<2>::cat({Automaton<2>::lit(0), Automaton<2>::lit(1)});
+	auto e = Automaton<2>::cat({Automaton<2>::lit(1), Automaton<2>::lit(0)});
+	auto f = Automaton<2>::alt({d, e});
+	auto fc = f->clone();
+	fc->totalize();
+	fc->determinize();
+	equivalentOnAllStrings<2>(f, fc, 8);
+	auto g = Automaton<2>::conj(f, d);
+	auto gc = g->clone();
+	gc->totalize();
+	gc->determinize();
+	equivalentOnAllStrings<2>(g, gc, 8);
 }
