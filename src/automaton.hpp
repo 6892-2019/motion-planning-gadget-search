f1a2d69f9a2a366defa2604e0334c2ae4bf467da
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2016-11-26 03:05:48 -0500

    vector-of-maps implementation
    We aren't actually using this because it doesn't pay off, but we'll keep
    it around to test again later.

--- src/automaton.hpp
+++ src/automaton.hpp
@@ -194,6 +194,50 @@ private:
 	using SparseConjMap = MapConjMap<google::sparse_hash_map<std::pair<state_type, state_type>,
 			state_type, boost::hash<std::pair<state_type, state_type>>>>;
 
+	/**
+	 * Implements a map of pairs as a vector of maps of elements.  On one hand,
+	 * our maps are smaller because each only has to store half the key; on the
+	 * other hand, the wasted space in one map can't be used by another, so our
+	 * total footprint may be larger.
+	 *
+	 * So far it doesn't seem to have worked out in either space or time, though
+	 * the latter might be fixed with a better hash function.
+	 */
+	class DenseConjVectorOfMaps {
+	public:
+		DenseConjVectorOfMaps(std::size_t leftSize, std::size_t rightSize) : maps_(std::min(leftSize, rightSize)), useLeft_(leftSize > rightSize) {
+			for (auto& map : maps_)
+				map.set_empty_key(std::numeric_limits<state_type>::max());
+		}
+		void insert(std::pair<state_type, state_type> oldstates, state_type newstate) {
+			if (useLeft_)
+				maps_[oldstates.second].insert({oldstates.first, newstate});
+			else
+				maps_[oldstates.first].insert({oldstates.second, newstate});
+		}
+		template<class Callable>
+		std::pair<state_type, bool> compute_if_absent(std::pair<state_type, state_type> oldstates, Callable newstateProvider) {
+			auto& map = useLeft_ ? maps_[oldstates.second] : maps_[oldstates.first];
+			state_type key = useLeft_ ? oldstates.first : oldstates.second;
+			auto it = map.find(key);
+			if (it != map.end())
+				return {it->second, false};
+			auto r = map.insert({key, newstateProvider()});
+			return {r.first->second, true};
+		}
+	private:
+		struct MyHash {
+			std::size_t operator()(const state_type s) const {
+				return (s * s) + s;
+//				return s ^ (s >> 8) ^ (s >> 16) ^ (s >> 24);
+			}
+		};
+		//both std::hash and boost::hash just return the state as its hash,
+		//which is very bad for dense_hash_map's power-of-two tables
+		std::vector<dense_hash_map<state_type, state_type, MyHash>> maps_;
+		bool useLeft_;
+	};
+
 	template <class Map>
 	static ptr conj_impl(const_ptr left, const_ptr right) {
 		//(left state, right state, new state)
