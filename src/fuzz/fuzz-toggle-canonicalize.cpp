63d8fbc71cfe279578864a90e22192ceb3bc6c36
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2017-04-19 18:11:23 -0400

    fuzz changes

--- src/fuzz/fuzz-toggle-canonicalize.cpp
+++ src/fuzz/fuzz-toggle-canonicalize.cpp
@@ -5,36 +5,48 @@
 using namespace automaton;
 
 int main(int argc, char* argv[]) {
-	std::mt19937 rng(10);
+	std::mt19937 rng(std::atoi(argv[1]));
 
-	Automaton<8> a = generate<8>(rng);
-	a.minimize();
-	canonicalize(a, a.alphabet_size());
-	std::vector<AutomatonBase::state_type> states;
-	std::vector<AutomatonBase::symbol_type> symbols;
-	std::uniform_int_distribution<> symbolRotateDistance(0, a.alphabet_size()-1);
+	for (int trials = 0; trials < 1; ++trials) {
+		auto randaut = generate<4>(rng);
 
-	int failures = 0;
-	for (int i = 0; i < 1000; ++i) {
+		std::cout << randaut << randaut.repr() << std::endl;
+		randaut.minimize();
+//		canonicalize(randaut, randaut.alphabet_size());
+		std::cout << randaut << std::endl;
+		std::vector<AutomatonBase::state_type> states;
 		states.assign(boost::make_counting_iterator<AutomatonBase::state_type>(0),
-			boost::make_counting_iterator<AutomatonBase::state_type>(a.state_size()));
-		symbols.assign(boost::make_counting_iterator<AutomatonBase::symbol_type>(0),
-			boost::make_counting_iterator<AutomatonBase::symbol_type>(a.alphabet_size()));
-		auto copy = a;
-		std::shuffle(states.begin(), states.end(), rng);
-		std::iter_swap(states.begin(), std::find(states.begin(), states.end(), 0));
-		std::rotate(symbols.begin(), symbols.begin()+symbolRotateDistance(rng), symbols.end());
-		copy.renumber(states.begin(), symbols.begin());
-		copy.minimize();
-		canonicalize(copy, copy.alphabet_size());
+				boost::make_counting_iterator<AutomatonBase::state_type>(randaut.state_size()));
+		std::vector<AutomatonBase::symbol_type> symbols;
 
-		auto cmp = compare_languages(a, copy);
-		//TODO: we actually need == (structural equality), but same-language is a good start!
-		if (!cmp.equal()) {
-			std::cout << "trial " << i << " found mismatch:\n" << cmp.leftButNotRight << "\n" << cmp.rightButNotLeft << std::endl;
-			if ((++failures) >= 5)
-				std::exit(0);
+		std::vector<Automaton<4>> rotations;
+		rotations.reserve(randaut.alphabet_size());
+		for (int rotDist = 0; rotDist < randaut.alphabet_size(); ++rotDist) {
+			auto copy = randaut;
+			symbols.assign(boost::make_counting_iterator<AutomatonBase::symbol_type>(0),
+					boost::make_counting_iterator<AutomatonBase::symbol_type>(randaut.alphabet_size()));
+			std::rotate(symbols.begin(), symbols.begin()+rotDist, symbols.end());
+			copy.renumber(states.begin(), symbols.begin());
+			std::cout << copy << std::endl;
+			for (auto x : symbols)
+				std::cout << x << " ";
+			std::cout << std::endl;
+//			copy.minimize();
+			canonicalize(copy, copy.alphabet_size());
+			std::cout << copy << std::endl;
+			rotations.push_back(std::move(copy));
+
+//			auto cmp = compare_languages(randaut, copy);
+//			//TODO: we actually need == (structural equality), but same-language is a good start!
+//			if (!cmp.equal()) {
+//				std::cout << "trial " << rotDist << " found mismatch:\n" << cmp.leftButNotRight << "\n" << cmp.rightButNotLeft << "\n" << std::endl;
+//			}
 		}
+
+		std::unordered_set<std::size_t> hashes;
+		for (const auto& r : rotations)
+			hashes.insert(std::hash<decltype(randaut)>()(r));
+		std::cout << hashes.size() << std::endl;
 	}
 
 	return 0;
