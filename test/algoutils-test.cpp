7a1e4b12741b693cb3d1cb3067d308c670834c69
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2017-04-23 01:37:12 -0400

    apply_reverse_permutation sanity test

--- test/algoutils-test.cpp
+++ test/algoutils-test.cpp
@@ -2,6 +2,18 @@
 #include "algoutils.hpp"
 #include <gtest/gtest.h>
 
+TEST(AlgoutilsTest, ApplyReversePermutationSanity) {
+	dynarray<int> foo(5), perm(5);
+	std::iota(foo.begin(), foo.end(), 0);
+	perm[0] = 1; perm[1] = 0; perm[2] = 4; perm[3] = 3; perm[4] = 2;
+	apply_reverse_permutation(foo.begin(), foo.end(), perm.begin());
+	EXPECT_EQ(foo[1], 0);
+	EXPECT_EQ(foo[0], 1);
+	EXPECT_EQ(foo[4], 2);
+	EXPECT_EQ(foo[3], 3);
+	EXPECT_EQ(foo[2], 4);
+}
+
 TEST(AlgoutilsTest, IsPossiblyMirroredRotationPermutation) {
 	ASSERT_TRUE(is_possibly_mirrored_rotation_permutation({0}));
 	ASSERT_FALSE(is_possibly_mirrored_rotation_permutation({1}));
