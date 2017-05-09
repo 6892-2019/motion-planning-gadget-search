8f68669b4f5db5816aeda6b646bdad5030e20e2f
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2017-05-09 13:40:37 -0400

    fastdebug is -O1, not -O2

--- genbuild.py
+++ genbuild.py
@@ -18,7 +18,7 @@ globals['testldflags'] = '$ldflags -lgtest -lgtest_main'
 
 debug_cfg = {'config': 'debug', 'optflags': '-g -O0 -march=native -gsplit-dwarf -fdebug-types-section -grecord-gcc-switches'}
 sanitize_cfg = {'config': 'sanitize', 'optflags': debug_cfg['optflags'] + ' -fsanitize=address -fsanitize=undefined'}
-fastdebug_cfg = {'config': 'fastdebug', 'optflags': '-g -O2 -march=native -gsplit-dwarf -fdebug-types-section -grecord-gcc-switches'}
+fastdebug_cfg = {'config': 'fastdebug', 'optflags': '-g -O1 -march=native -gsplit-dwarf -fdebug-types-section -grecord-gcc-switches'}
 release_cfg = {'config': 'release', 'optflags': '-O2 -march=native -flto -fvisibility=hidden -DNDEBUG'}
 configs = [debug_cfg, sanitize_cfg, fastdebug_cfg, release_cfg]
 
