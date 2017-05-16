433bed9aff1d425fedc7190b23ac251f87099430
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2017-05-16 02:32:49 -0400

    Move Tarjan implementation to .cpp

--- src/automaton.cpp
+++ src/automaton.cpp
@@ -28,6 +28,55 @@ std::ostream& operator<<(std::ostream& os, const AutomatonReprStreamer& rs) {
 	os << "}";
 	return os;
 }
+
+struct Tarjan {
+	Tarjan(const AutomatonBase& a_) : a(a_), lowlink(a.state_size()), number(a.state_size()) {
+		std::fill(number.begin(), number.end(), std::numeric_limits<state_type>::max());
+		result.components_.reserve(a.state_size());
+	}
+	const AutomatonBase& a;
+	unsigned int index;
+	std::vector<state_type> stack;
+	dynarray<state_type> lowlink, number;
+	SCCs result;
+
+	void strongconnect(state_type v) {
+		lowlink[v] = number[v] = index++;
+		stack.push_back(v);
+		a.for_each_destination(v, [&](state_type w) {
+			if (number[w] == std::numeric_limits<state_type>::max()) {
+				strongconnect(w);
+				//TODO: apparently lowlink is only used in strongconnect(v), so
+				//we could make it the return value instead of an array
+				lowlink[v] = std::min(lowlink[v], lowlink[w]);
+			} else if (number[w] < number[v] && std::find(stack.begin(), stack.end(), w) != stack.end())
+				lowlink[v] = std::min(lowlink[v], number[w]);
+		});
+		if (lowlink[v] == number[v]) {
+			result.indices_.push_back(static_cast<unsigned int>(result.components_.size()));
+			while (!stack.empty() && number[stack.back()] >= number[v]) {
+				result.components_.push_back(stack.back());
+				stack.pop_back();
+			}
+		}
+	}
+	SCCs compute() {
+		for (state_type w = 0; w < a.state_size(); ++w)
+			if (number[w] == std::numeric_limits<state_type>::max())
+				strongconnect(w);
+		result.indices_.push_back(static_cast<unsigned int>(result.components_.size()));
+
+		for (state_type s = 0; s < a.state_size(); ++s)
+			//Each state is in exactly one component.
+			assert(std::count(result.components_.begin(), result.components_.end(), s) == 1);
+
+		return std::move(result);
+	}
+};
 } //namespace detail
+
+SCCs find_components(const AutomatonBase& a) {
+	return detail::Tarjan(a).compute();
+}
 
 } //namespace automaton
