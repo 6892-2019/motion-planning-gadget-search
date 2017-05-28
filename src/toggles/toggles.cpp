e3f16bb3fa00cf1d83b7f5113bc9781a73a0e7c6
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2017-05-27 20:37:48 -0400

    Only issue connect if it's pending

--- src/toggles/toggles.cpp
+++ src/toggles/toggles.cpp
@@ -306,7 +306,7 @@ void registrar_thread(int core_number) {
 	auto lastReport = std::chrono::steady_clock::now();
 	//Steady state: alternate issuance and retirement, periodically issuing a connect task
 	while (registry.waiting_size() || connect_pending() || inflight) {
-		if (connectibles.size() >= 100 || !registry.waiting_size())
+		if (connect_pending() && (connectibles.size() >= 100 || !registry.waiting_size()))
 			issue_connect();
 		else while (registry.waiting_size())
 			if (try_issue_combine())
