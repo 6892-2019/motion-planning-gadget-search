96637c58fed0496d4bd26c67fe9ab1f5038a5875
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2017-06-04 02:52:49 -0400

    Split automaton-instantiations.cpp in half
    This is basically just as effective as you'd think.

---
+++ src/automaton-instantiations-0.cpp
@@ -0,0 +1,11 @@
+#include "precompiled.hpp"
+#include "automaton.hpp"
+
+template class automaton::Automaton<1u>;
+template class automaton::Automaton<2u>;
+template class automaton::Automaton<3u>;
+template class automaton::Automaton<4u>;
+template class automaton::Automaton<5u>;
+template class automaton::Automaton<6u>;
+template class automaton::Automaton<7u>;
+template class automaton::Automaton<8u>;
--- src/automaton-instantiations-1.cpp
+++ src/automaton-instantiations-1.cpp
@@ -1,14 +1,6 @@
 #include "precompiled.hpp"
 #include "automaton.hpp"
 
-template class automaton::Automaton<1u>;
-template class automaton::Automaton<2u>;
-template class automaton::Automaton<3u>;
-template class automaton::Automaton<4u>;
-template class automaton::Automaton<5u>;
-template class automaton::Automaton<6u>;
-template class automaton::Automaton<7u>;
-template class automaton::Automaton<8u>;
 template class automaton::Automaton<9u>;
 template class automaton::Automaton<10u>;
 template class automaton::Automaton<11u>;
