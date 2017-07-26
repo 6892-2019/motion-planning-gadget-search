7c909af4609dd74f888c0caa89b4a55e75ed6751
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2017-07-26 01:14:33 -0400

    Define seven-tripwire via new mechanism

--- src/toggles/gadgetdefs.cpp
+++ src/toggles/gadgetdefs.cpp
@@ -41,10 +41,39 @@ automaton_type prepare(automaton_type a) {
 	return a;
 }
 
+automaton_type make_twostate(automaton_type&& trans00, automaton_type&& trans01,
+		automaton_type&& trans10, automaton_type&& trans11) {
+	automaton_type a;
+	a.addState(); a.addState();
+	a.setAccept(0); a.setAccept(1);
+
+	auto do_trans = [&](automaton_type::state_type from, automaton_type::state_type to, auto&& trans) {
+		auto base = a.append(std::move(trans));
+		a.addEpsilon(from, base);
+		for (auto i = base; i < a.state_size(); ++i)
+			if (a.accept(i)) {
+				a.setAccept(i, false);
+				a.addEpsilon(i, to);
+			}
+	};
+	do_trans(0, 0, std::move(trans00));
+	do_trans(0, 1, std::move(trans01));
+	do_trans(1, 0, std::move(trans10));
+	do_trans(1, 1, std::move(trans11));
+
+	return prepare(a);
+}
+
+automaton_type make_twostate(automaton_type&& trans01, automaton_type&& trans10) {
+	auto empty = [](){return automaton::empty<automaton_type::alphabet_size_v>();};
+	return make_twostate(empty(), std::move(trans01), std::move(trans10), empty());
+}
+
 std::unordered_map<std::string, automaton_type> initialize_known_gadgets() {
 	std::unordered_map<std::string, automaton_type> ret;
 	constexpr unsigned int N = automaton_type::alphabet_size_v;
 	auto lit = [](auto... symbols){return automaton::lit<N>(symbols...);};
+	auto empty = [](){return automaton::empty<automaton_type::alphabet_size_v>();};
 
 	ret["2-nop"] = prepare(make_nop(2));
 	ret["3-nop"] = prepare(make_nop(3));
@@ -52,10 +81,6 @@ std::unordered_map<std::string, automaton_type> initialize_known_gadgets() {
 
 	ret["split"] = prepare(star(nCopies(alt(lit(0), lit(1), lit(2)), 2)));
 
-	auto make_twostate = [&](auto&& state0, auto&& state1) {
-		auto base = alt(epsilon<N>(), state0, star(cat(state0, state1)), cat(state0, star(cat(state1, state0))));
-		return prepare(base);
-	};
 	ret["1-toggle"] = make_twostate(lit(0, 1), lit(1, 0));
 	ret["parallel-2-toggle"] = make_twostate(alt(lit(0, 1), lit(3, 2)), alt(lit(1, 0), lit(2, 3)));
 	ret["antiparallel-2-toggle"] = make_twostate(alt(lit(0, 1), lit(2, 3)), alt(lit(1, 0), lit(3, 2)));
@@ -86,6 +111,12 @@ std::unordered_map<std::string, automaton_type> initialize_known_gadgets() {
 			alt(lit(1, 0), lit(2, 1), lit(0, 2)));
 	ret["4-spinner"] = make_twostate(alt(lit(0, 1), lit(1, 2), lit(2, 3), lit(3, 0)),
 			alt(lit(1, 0), lit(2, 1), lit(3, 2), lit(0, 3)));
+
+	ret["seven-tripwire"] = make_twostate(
+			empty(),
+			alt(lit(0, 1), lit(2, 3), lit(3, 2)),
+			alt(lit(1, 0), lit(2, 3), lit(3, 2)),
+			lit(1, 2));
 
 	return ret;
 }
