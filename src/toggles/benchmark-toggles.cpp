86fdc4f537070d4ca4334e7131396f3be424ebb9
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2018-05-30 00:18:33 -0400

    Combine-ish benchmark in benchmark-toggles

--- src/toggles/benchmark-toggles.cpp
+++ src/toggles/benchmark-toggles.cpp
@@ -25,28 +25,6 @@ using ClosedSet = tsl::hopscotch_set<PackPointer,
 using NonowningClosedSet = ClosedSet<const PackedAutomaton*>;
 
 namespace {
-struct Finisher {
-	Finisher() : pruned(0) {}
-	vector<unique_ptr<const PackedAutomaton>> nextgen;
-	NonowningClosedSet closed;
-	unsigned int pruned;
-	void operator()(automaton_type&& a, Provenance p) {
-		canonicalize(a, a.active_alphabet_size());
-		auto packed = pack(a);
-		auto hash = packed->packed_hash();
-		if (closed.insert(packed.get()).second) { //TODO: insert overload taking the hash
-			nextgen.emplace_back(std::move(packed));
-		} else
-			++pruned;
-	}
-};
-
-void connect_once(const PackedAutomaton* source, index_type sourceIndex, Finisher& finishAction) {
-	automaton_type inflated(*source);
-	automaton_type::symbol_type locations = inflated.active_alphabet_size();
-	connect(inflated, sourceIndex, false, locations, finishAction);
-}
-
 vector<unique_ptr<WorkingAutomaton>> load_automata(const char** first, const char** last) {
 	vector<string> filenames = processFilenameArgs(first, last);
 	vector<unique_ptr<WorkingAutomaton>> result;
@@ -63,6 +41,78 @@ vector<unique_ptr<const PackedAutomaton>> pack_all(const std::vector<unique_ptr<
 		p.push_back(pack(trash));
 	}
 	return p;
+}
+
+struct Finisher {
+	Finisher() : pruned(0) {}
+	vector<unique_ptr<const PackedAutomaton>> nextgen;
+	NonowningClosedSet closed;
+	unsigned int pruned;
+	void operator()(automaton_type&& a, Provenance p) {
+		canonicalize(a, a.active_alphabet_size());
+		auto packed = pack(a);
+		auto hash = packed->packed_hash();
+		if (closed.insert(packed.get()).second) { //TODO: insert overload taking the hash
+			nextgen.emplace_back(std::move(packed));
+		} else
+			++pruned;
+	}
+};
+
+void combine_once(const PackedAutomaton* left, const PackedAutomaton* rightA, Finisher& finishAction) {
+	automaton_type unpacked(*left);
+	//TODO: we don't usually have to unpack or mirror the inputs, so this isn't
+	//a perfectly accurate benchmark.
+	automaton_type right(*rightA);
+	automaton_type::symbol_type leftLocations = unpacked.active_alphabet_size();
+	automaton_type::symbol_type rightLocations = right.active_alphabet_size();
+	if (leftLocations + rightLocations > automaton_type::alphabet_size_v) return;
+	automaton_type mirrored = mirror(unpacked), rightMir = mirror(right);
+	bool shouldmirror = unpacked == mirrored;
+	combine(unpacked, 0, false, leftLocations, right, 0, false, rightLocations, finishAction);
+	if (right != rightMir)
+		combine(unpacked, 0, false, leftLocations, rightMir, 0, true, rightLocations, finishAction);
+	if (shouldmirror) {
+		combine(mirrored, 0, true, leftLocations, right, 0, false, rightLocations, finishAction);
+		if (right != rightMir) //TODO: the both-mirrored combine may be redundant
+			combine(mirrored, 0, true, leftLocations, rightMir, 0, true, rightLocations, finishAction);
+	}
+}
+
+int benchmark_combine(int argc, const char* argv[]) {
+	if (argc < 3) {
+		std::cout << "specify at least one automaton file or response file\n";
+		return 1;
+	}
+	auto packed = pack_all(load_automata(&argv[2], &argv[argc]));
+	std::cout << "loaded " << packed.size() << " automata\n";
+	Finisher finisher;
+
+	auto start = myclock::now();
+	for (const auto& p : packed)
+		for (const auto& q : packed)
+			combine_once(p.get(), q.get(), finisher);
+	auto end = myclock::now();
+
+	auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::size_t hash = 0;
+	//Print the hash a) to prevent the benchmark from being optimized out and
+	//b) so we can tell if our optimizations changed the result or not.
+	for (auto& pa : finisher.nextgen) {
+		hash += pa->packed_hash();
+		std::cout << pa->accept_size() << std::endl;
+	}
+	std::cout << elapsed << " microseconds, "
+			<< finisher.nextgen.size() << " results, "
+			<< finisher.pruned << " pruned, "
+			<< hash << std::endl;
+	return 0;
+}
+
+void connect_once(const PackedAutomaton* source, index_type sourceIndex, Finisher& finishAction) {
+	automaton_type inflated(*source);
+	automaton_type::symbol_type locations = inflated.active_alphabet_size();
+	connect(inflated, sourceIndex, false, locations, finishAction);
 }
 
 int benchmark_connect(int argc, const char* argv[]) {
@@ -141,7 +191,9 @@ int main(int argc, const char* argv[]) { //genbuild entrypoint
 		return 1;
 	}
 
-	if (argv[1] == "connect"sv)
+	if (argv[1] == "combine"sv)
+		return benchmark_combine(argc, argv);
+	else if (argv[1] == "connect"sv)
 		return benchmark_connect(argc, argv);
 	else if (argv[1] == "mirror"sv)
 		return benchmark_mirror(argc, argv);
