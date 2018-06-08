d0e891b9a77911dd6647ccf58e6c19f412fbae3f
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2018-06-08 01:13:25 -0400

    Stop checking strings in equivalentOnAllStrings
    It's pointless now that we're comparing the entire language.  If we need
    witnesses, we should write some code that prints the smallest 10 strings
    from each of the ComparisonResult automata.

--- src/test/util.hpp
+++ src/test/util.hpp
@@ -16,16 +16,9 @@ std::vector<std::vector<unsigned int>> allStrings(unsigned int alphabetSize, uns
  * if the automata may differ on longer strings.)
  */
 template<unsigned int AlphabetSize>
-void equivalentOnAllStrings(const automaton::Automaton<AlphabetSize>& a, const automaton::Automaton<AlphabetSize>& b, int length, int lineno = -1) {
-	for (auto& string : allStrings(AlphabetSize, length))
-		//CHECK_EQ_MESSAGE(a.run(string), b.run(string), to_string(string) << " from line " << lineno);
-		CHECK_EQ(a.run(string), b.run(string));
+void equivalentOnAllStrings(const automaton::Automaton<AlphabetSize>& a, const automaton::Automaton<AlphabetSize>& b, int, int = -1) {
 	automaton::ComparisonResult<AlphabetSize> cmp = compare_languages(a, b);
-	//CHECK_MESSAGE(cmp.equal(), " from line " << lineno << "\n" << cmp.leftButNotRight << '\n' << cmp.rightButNotLeft << std::endl);
-	CHECK_UNARY(cmp.equal());
-	//TODO: make these const
-//	EXPECT_EQ(a.isEmpty(), b.isEmpty()) << " from line " << lineno;
-//	EXPECT_EQ(a.infinite(), b.infinite()) << " from line " << lineno;
+	CHECK_UNARY(cmp.equal()); //TODO: on failure, print some witnesses
 }
 
 #endif // AUTOMATON_TEST_UTIL_HPP_INCLUDED
