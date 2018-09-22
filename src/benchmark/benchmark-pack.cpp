9e8e8bb4883ac667a01e834c846c97595638f7a4
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2018-09-22 03:44:40 -0400

    Update benchmark-pack to new pack scheme
    As a bonus, we can now easily print the total size of the packs.  We
    might want to print the size of each pack in some diff-friendly format
    so we can track the benefit of new coding flags.  (The format for binary
    size diffs, used in earlier commit messages, would work well.)

--- src/benchmark/benchmark-pack.cpp
+++ src/benchmark/benchmark-pack.cpp
@@ -2,7 +2,7 @@
 #include "automatonbase.hpp"
 #include "automaton.hpp"
 #include "automaton-io.hpp"
-#include "packedautomaton.hpp"
+#include "pack.hpp"
 
 using namespace automaton;
 //'clock' in the global namespace is already defined, sigh
@@ -13,20 +13,25 @@ int main(int argc, char* argv[]) { //genbuild entrypoint
 	for (int i = 1; i < argc; ++i)
 		automata.push_back(deserialize(argv[i]));
 
-	std::vector<std::unique_ptr<const PackedAutomaton>> packs;
+	dynarray<std::byte> data(1024*1024*1024);
+	std::vector<const Pack*> packs;
 	packs.reserve(automata.size());
+	std::byte* cur = data.begin();
 	auto start = myclock::now();
-	for (auto& pa : automata)
-		packs.push_back(pack(*pa));
+	for (auto& pa : automata) {
+		packs.push_back(cur);
+		cur = pack(*pa, cur, data.end());
+	}
 	auto end = myclock::now();
 
 	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "\n";
 	std::size_t hash = 0;
 	//Print the hash a) to prevent the benchmark from being optimized out and
 	//b) so we can tell if our optimizations changed the result or not.
-	for (auto& pa : packs)
-		hash += pa->packed_hash();
+	for (auto p : packs)
+		hash += packed_hash(p);
 	std::cout << hash << "\n";
+	std::cout << (cur - data.begin()) << "\n";
 
 	return 0;
 }
