8cffcec0cf469b0b38f8931ac201c05cb17de402
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2018-12-23 23:03:56 -0500

    Correct some lock-related gadgetdef mistakes

--- src/toggles/gadgetdefs.cpp
+++ src/toggles/gadgetdefs.cpp
@@ -163,20 +163,20 @@ const GadgetLine crossing_2_toggle[] = {
 	{1, 2, 0, 0}, {1, 1, 3, 0},
 };
 const GadgetLine noncrossing_tripwire_lock[] = {
-	{0, 0, 1, 1}, {0, 1, 0, 1}, {0, 2, 3, 1}, {0, 3, 2, 1},
+	{0, 0, 1, 1}, {0, 1, 0, 1}, {0, 2, 3, 0}, {0, 3, 2, 0},
 	{1, 0, 1, 0}, {1, 1, 0, 0},
 };
 const GadgetLine crossing_tripwire_lock[] = {
-	{0, 0, 2, 1}, {0, 2, 0, 1}, {0, 1, 3, 1}, {0, 3, 1, 1},
+	{0, 0, 2, 1}, {0, 2, 0, 1}, {0, 1, 3, 0}, {0, 3, 1, 0},
 	{1, 0, 2, 0}, {1, 2, 0, 0},
 };
 const GadgetLine noncrossing_toggle_lock[] = {
-	{0, 0, 1, 1}, {0, 2, 3, 1}, {0, 3, 2, 1},
+	{0, 0, 1, 1}, {0, 2, 3, 0}, {0, 3, 2, 0},
 	{1, 1, 0, 0},
 };
 const GadgetLine crossing_toggle_lock[] = {
-	{0, 0, 2, 1}, {0, 1, 3, 1}, {0, 3, 1, 1},
-	{1, 2, 0, 1},
+	{0, 0, 2, 1}, {0, 1, 3, 0}, {0, 3, 1, 0},
+	{1, 2, 0, 0},
 };
 const GadgetLine noncrossing_tripwire_toggle[] = {
 	{0, 0, 1, 1}, {0, 2, 3, 1}, {0, 3, 2, 1},
