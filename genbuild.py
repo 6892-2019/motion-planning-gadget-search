2dce3b4ee389cf40d9d6ac1db8a634f02d79bf33
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2019-02-01 23:56:57 -0500

    Link with gold and --gdb-index
    gold links faster.  We also need the --gdb-index flag for -fsplit-dwarf
    to actually work (as opposed to just concatenating sections); this saves
    ~8MB for toggles-runner.exe.
    
    gdb can't actually use this debug info due to a binutils bug.

--- genbuild.py
+++ genbuild.py
@@ -14,7 +14,7 @@ global_flags = OrderedDict()
 global_flags['builddir'] = 'build' # significant to Ninja
 global_flags['modeflags'] = '-std=c++1z'
 global_flags['warnflags'] = '-pedantic -Wall -Wextra -Wuninitialized -Winit-self -Wconversion -Wuseless-cast -Wlogical-op -Waggressive-loop-optimizations -Winvalid-pch -Wno-unused-parameter -Wduplicated-cond -Wnull-dereference -Wno-dangling-else -Wsuggest-override -fdiagnostics-color=always'
-global_flags['ldflags'] = '-u malloc -ljemalloc_pic -lc -lpthread -ltbb -ldl -l:libboost_system.a -lsctp -Wl,--gc-sections'
+global_flags['ldflags'] = '-fuse-ld=gold -Wl,--gc-sections -Wl,--gdb-index -u malloc -ljemalloc_pic -lc -lpthread -ltbb -ldl -l:libboost_system.a -lsctp'
 
 # configuration-specific flag *templates*
 config_flags = OrderedDict()
