c517c2023fba9f6b9d571a8fc8d971dfd490ceb6
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2019-09-11 04:07:32 -0400

    Add assertion in decode_skinny_edge_page

--- src/toggles/toggles-shared.cpp
+++ src/toggles/toggles-shared.cpp
@@ -143,6 +143,9 @@ std::uint64_t decode_skinny_edge_page(std::string_view key, std::string_view val
 	offsets.push_back(length_of_offsets);
 	while (p != first + length_of_offsets)
 		offsets.push_back(offsets.back() + numeric_cast<std::uint16_t>(upv::read(p))); //remaining elements are list lengths, so sum to get offset
+	//Strictly speaking, the last length could be omitted because it's implied
+	//by the length of the value.
+	assert(offsets.back() == value.length());
 	uint64_t last_id_on_page = lmdb::from_sv<uint64_t>(key);
 	return last_id_on_page - (offsets.size()-1) + 1;
 }
