20c29fa379610bb68adbf5823a32bcde1554b1b6
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2019-09-17 03:07:07 -0400

    proj_compare: coord_equal_left/right

--- src/proj_compare.hpp
+++ src/proj_compare.hpp
@@ -46,6 +46,24 @@ struct coord_less_right {
 	}
 };
 
+template<int I>
+struct coord_equal_left {
+	template<typename T, typename U>
+	bool operator()(const T& l, const U& r) const {
+		using std::get;
+		return get<I>(l) == r;
+	}
+};
+
+template<int I>
+struct coord_equal_right {
+	template<typename T, typename U>
+	bool operator()(const U& l, const T& r) const {
+		using std::get;
+		return l == get<I>(r);
+	}
+};
+
 //We could add variadic proj_compare_left/right that expects U to be an appropriate tuple.
 
 #endif /* COMPARE_PROJ_HPP */
