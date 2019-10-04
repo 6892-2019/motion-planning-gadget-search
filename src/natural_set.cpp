78dbf4b34610356dd919b37e33dc6c1dc4de3b08
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2019-10-04 01:38:36 -0400

    Remove unused natural_set, natural_map

--- src/natural_set.cpp
+++
@@ -1,7 +0,0 @@
-#include "natural_set.hpp"
-
-template class natural_set<unsigned char>;
-template class natural_set<unsigned short>;
-template class natural_set<unsigned int>;
-template class natural_set<unsigned long>;
-template class natural_set<unsigned long long>;
