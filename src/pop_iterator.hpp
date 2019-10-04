6e7817d7e94638359c03bd13ad1cd1dbab33e753
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2019-10-04 01:21:23 -0400

    Possible refactor to support pop_back_iterator
    I don't like this.  It doesn't seem to save us much repetition and also
    allows pop_front_iterator and pop_back_iterator to compare equal if they
    point at the same container or are both sentinels.
    
    Maybe parameterizing pop_iterator with function/member-function pointers
    would work better, or having a separate traits template.

--- src/pop_iterator.hpp
+++ src/pop_iterator.hpp
@@ -5,8 +5,9 @@
 #include <type_traits>
 #include <cassert>
 
+//namespace detail {
 template<class Container>
-class pop_front_iterator {
+class pop_iterator {
 public:
 	using iterator_category = std::input_iterator_tag;
 	using value_type = typename Container::value_type;
@@ -16,20 +17,10 @@ public:
 	using pointer = value_type*; //dangerous: there are no "rvalue pointers"
 	using difference_type = std::ptrdiff_t;
 
-	pop_front_iterator() : c(nullptr) {}
-	pop_front_iterator(Container& container) : c(std::addressof(container)) {}
+	pop_iterator() : c(nullptr) {}
+	pop_iterator(Container& container) : c(std::addressof(container)) {}
 	//implicit copy and move
 
-	reference operator*() {
-		return static_cast<reference>(c->front());
-	}
-	pointer operator->() {
-		return &(c->front());
-	}
-	pop_front_iterator& operator++() {
-		c->pop_front();
-		return *this;
-	}
 	void operator++(int) {
 		this->operator++();
 		//We deliberately do not return a copy of the iterator before the change
@@ -39,7 +30,7 @@ public:
 	}
 
 	//Not required for input iterators, so unlikely to be used, but why not?
-	difference_type operator-(const pop_front_iterator& o) const {
+	difference_type operator-(const pop_iterator& o) const {
 		if (*this == o) return 0;
 		if (c) return c->size();
 		if (o.c) return -((difference_type)o.c->size());
@@ -47,15 +38,43 @@ public:
 		__builtin_unreachable();
 	}
 
-	bool operator==(const pop_front_iterator& o) const {
+	bool operator==(const pop_iterator& o) const {
 		assert(!c || !o.c || c == o.c); //if both not nullptr, must point to same container
 		return c == o.c || (c && c->empty()) || (o.c && o.c->empty());
 	}
-	bool operator!=(const pop_front_iterator& o) const {
+	bool operator!=(const pop_iterator& o) const {
 		return !(*this == o);
 	}
-private:
+protected:
 	Container* c;
+};
+//}
+
+template<class Container>
+class pop_front_iterator : public pop_iterator<Container> {
+private:
+	using base_type = pop_iterator<Container>;
+public:
+	using iterator_category = typename base_type::iterator_category;
+	using value_type = typename base_type::value_type;
+	using reference = typename base_type::reference;
+	using pointer = typename base_type::pointer;
+	using difference_type = typename base_type::difference_type;
+
+	pop_front_iterator() : base_type() {}
+	pop_front_iterator(Container& container) : base_type(container) {}
+	//implicit copy and move
+
+	reference operator*() {
+		return static_cast<reference>(this->c->front());
+	}
+	pointer operator->() {
+		return &(this->c->front());
+	}
+	pop_front_iterator& operator++() {
+		this->c->pop_front();
+		return *this;
+	}
 };
 
 //https://stackoverflow.com/questions/47134311/how-to-implement-stddistance-for-custom-templated-iterator
--- src/test/pop_iterator-test.cpp
+++ src/test/pop_iterator-test.cpp
@@ -2,13 +2,19 @@
 #include "pop_iterator.hpp"
 #include "doctest.h"
 
-TEST_CASE("PopFrontIterator_DequeMerge") {
-	std::deque<int> left = {1, 2, 5, 6, 10};
-	std::deque<int> right = {3, 4, 7, 8, 9};
-	std::deque<int> actual;
-	std::merge(pop_front_begin(left), pop_front_end(left), pop_front_begin(right), pop_front_end(right), std::back_inserter(actual));
-	CHECK_UNARY(left.empty());
-	CHECK_UNARY(right.empty());
-	std::initializer_list<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	CHECK_UNARY(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+//TEST_CASE("PopFrontIterator_DequeMerge") {
+//	std::deque<int> left = {1, 2, 5, 6, 10};
+//	std::deque<int> right = {3, 4, 7, 8, 9};
+//	std::deque<int> actual;
+//	std::merge(pop_front_begin(left), pop_front_end(left), pop_front_begin(right), pop_front_end(right), std::back_inserter(actual));
+//	CHECK_UNARY(left.empty());
+//	CHECK_UNARY(right.empty());
+//	std::initializer_list<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//	CHECK_UNARY(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+//}
+
+TEST_CASE("PopFront_asdfd") {
+	std::deque<int> foo = {1, 2, 3};
+	pop_front_iterator i(foo);
+	*i;
 }
