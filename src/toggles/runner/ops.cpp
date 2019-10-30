6892-2019/motion-planning-gadget-search

commit f6ed78b91727a6a1677e347233736721d3d9c4be

Compute active alphabet while looking for nop locations
This fixes a minor bug in the previous commit (involving bitset::size
versus bitset::count), but there is no change to the resulting output,
just some saved work.

The remaining inefficiencies are that AutomatonBase::edges and
Automaton::labels return SymbolSets that we then immediately convert
back to bitsets.

src/toggles/runner/ops.cpp
@@ -96,7 +96,7 @@ auto connect_alphamap(unsigned int locations, unsigned int connectPoint) {
 }
 
 template<unsigned int N, class Provenance>
-AutomatonBase::SymbolSet connect_at(const Automaton<N>& a, unsigned int activeAlphabetSize,
+void connect_at(const Automaton<N>& a, unsigned int activeAlphabetSize,
 		Provenance prov, Finisher<Provenance>& finisher) {
 	Automaton<N> connected = a;
 	enjoin(connected, prov.connectPoint, (prov.connectPoint+1) % activeAlphabetSize);
@@ -105,17 +105,16 @@ AutomatonBase::SymbolSet connect_at(const Automaton<N>& a, unsigned int activeAl
 	connected.renumberAlphabet(alphamap.begin());
 
 	connected.minimize();
-	auto active = connected.activeAlphabet();
-	if (active.size() <= 1) return {}; //there are no interesting 1-symbol automata
 
 	using state_type = typename WorkingAutomaton::state_type;
 	using symbol_type = typename WorkingAutomaton::symbol_type;
-	bitset<N> known_not_nop;
+	bitset<N> active, known_not_nop;
 	boost::container::small_vector<pair<state_type, symbol_type>, 16> check_again;
 	//Strictly speaking, we only need two bits (0, 1, >1) but a saturating 2-bit
 	//counter is nontrivial to implement.
 	std::vector<unsigned int> indegree(connected.state_size(), 0);
-	for (state_type s = 0, end = connected.state_size(); s < end && known_not_nop.size() != active.size(); ++s) {
+	for (state_type s = 0, end = connected.state_size(); s < end && (known_not_nop & active).count() != (activeAlphabetSize-2); ++s) {
+		active |= connected.outgoing_mask(s);
 		if (!connected.accept(s)) continue;
 		for (auto&& [symbols, next] : connected.edges(s)) {
 			indegree[next] += symbols.size();
@@ -146,20 +145,16 @@ AutomatonBase::SymbolSet connect_at(const Automaton<N>& a, unsigned int activeAl
 		//If i->second was later found to be not-nop, this won't change anything.
 		if (indegree[i->first] != 1)
 			known_not_nop.set(i->second);
+	active &= known_not_nop;
 
-	if (known_not_nop.size() != active.size()) {
-		for (unsigned int i = active.size(); i-- > 0;)
-			if (!known_not_nop[active[i]])
-				active.erase(active.begin()+i);
-		if (active.size() <= 1) return {}; //there are no interesting 1-symbol automata
-	}
-
-	if (active.size() != (activeAlphabetSize - 2)) {
+	if (active.count() <= 1) return; //there are no interesting 1-symbol automata
+	if (active.count() != (activeAlphabetSize - 2)) {
 		//compress the alphabet
-		active.sort();
-		std::array<typename Automaton<N>::symbol_type, Automaton<N>::alphabet_size_v> compression;
-		std::copy(active.begin(), active.end(), compression.begin());
-		std::fill(compression.begin()+active.size(), compression.end(), std::numeric_limits<typename Automaton<N>::symbol_type>::max());
+		std::array<symbol_type, Automaton<N>::alphabet_size_v> compression;
+		auto i = compression.begin();
+		for (unsigned int b = active.find_first(); b < active.size(); b = active.find_next(b))
+			*i++ = b;
+		std::fill(i, compression.end(), std::numeric_limits<typename Automaton<N>::symbol_type>::max());
 		connected.renumberAlphabet(compression.begin());
 		//If we only deleted unused symbols, we don't need to minimize again;
 		//any two equivalent states would differ only in the symbols we deleted,
@@ -169,7 +164,6 @@ AutomatonBase::SymbolSet connect_at(const Automaton<N>& a, unsigned int activeAl
 	}
 
 	finisher(std::move(connected), prov);
-	return active;
 }
 
 template<unsigned int N>
@@ -225,9 +219,8 @@ AutomatonBase::SymbolSet connect_deleted_symbols(const AutomatonBase& a, unsigne
 	Finisher<ConnectProvenance> finisher;
 	auto active_alphabet_size = a.active_alphabet_size();
 
-	AutomatonBase::SymbolSet active;
 	switch (a.alphabet_size()) {
-#define TOGGLESRUNNER_CONNECT_CASE(N) case N: active = connect_at(static_cast<const Automaton<N>&>(a), active_alphabet_size, prov, finisher); break;
+#define TOGGLESRUNNER_CONNECT_CASE(N) case N: connect_at(static_cast<const Automaton<N>&>(a), active_alphabet_size, prov, finisher); break;
 		TOGGLESRUNNER_CONNECT_CASE(4)
 		TOGGLESRUNNER_CONNECT_CASE(5)
 		TOGGLESRUNNER_CONNECT_CASE(6)
@@ -248,7 +241,7 @@ AutomatonBase::SymbolSet connect_deleted_symbols(const AutomatonBase& a, unsigne
 			std::terminate();
 	}
 
-	AutomatonBase::SymbolSet deleted;
+	AutomatonBase::SymbolSet active = a.activeAlphabet(), deleted;
 	if (active_alphabet_size < 2) return deleted; //can't happen?
 	//We should now be dense for 0..alpha-2.
 	for (unsigned int i = 0; i < active_alphabet_size-2; ++i)

commit 3964f5b7e69cafc5fcd40ba9d36338e96d86828d

Fix deleted locations reporting (broken in f6ed78b)
f6ed78b changed connect_at to not return the active alphabet, which is
good because we only need it for reporting deleted locations.  But
connect_at copies its input automaton instead of modifying it in place,
and the adjusted code in connect_deleted_symbols checked the input
automaton, so we never saw any deleted locations.

We now return a bitset, which should be cheap, and decode it only when
we're reporting.  This shouldn't impact actual connects.

This caused some reports on archived databases to be wrong.

src/toggles/runner/ops.cpp
@@ -96,7 +96,7 @@ auto connect_alphamap(unsigned int locations, unsigned int connectPoint) {
 }
 
 template<unsigned int N, class Provenance>
-void connect_at(const Automaton<N>& a, unsigned int activeAlphabetSize,
+bitset<N> connect_at(const Automaton<N>& a, unsigned int activeAlphabetSize,
 		Provenance prov, Finisher<Provenance>& finisher) {
 	Automaton<N> connected = a;
 	enjoin(connected, prov.connectPoint, (prov.connectPoint+1) % activeAlphabetSize);
@@ -148,7 +148,7 @@ void connect_at(const Automaton<N>& a, unsigned int activeAlphabetSize,
 			known_not_nop.set(i->second);
 	active &= known_not_nop;
 
-	if (active.count() <= 1) return; //there are no interesting 1-symbol automata
+	if (active.count() <= 1) return {}; //there are no interesting 1-symbol automata
 	if (active.count() != (activeAlphabetSize - 2)) {
 		//compress the alphabet
 		std::array<symbol_type, Automaton<N>::alphabet_size_v> compression;
@@ -165,6 +165,7 @@ void connect_at(const Automaton<N>& a, unsigned int activeAlphabetSize,
 	}
 
 	finisher(std::move(connected), prov);
+	return active;
 }
 
 template<unsigned int N>
@@ -214,14 +215,17 @@ void connect(const AutomatonBase& a, std::uint64_t input1, Finisher<ConnectProve
 }
 
 AutomatonBase::SymbolSet connect_deleted_symbols(const AutomatonBase& a, unsigned int connectPoint) {
-	//This is to avoid another instantiation of connect_at.
+	//This is to avoid another instantiation of connect_at.  We do waste some
+	//time minimizing and canonicalizing the automaton.
 	ConnectProvenance prov;
 	prov.connectPoint = numeric_cast<std::uint8_t>(connectPoint);
 	Finisher<ConnectProvenance> finisher;
 	auto active_alphabet_size = a.active_alphabet_size();
+	if (active_alphabet_size < 2) return {}; //can't happen?
+	AutomatonBase::SymbolSet active;
 
 	switch (a.alphabet_size()) {
-#define TOGGLESRUNNER_CONNECT_CASE(N) case N: connect_at(static_cast<const Automaton<N>&>(a), active_alphabet_size, prov, finisher); break;
+#define TOGGLESRUNNER_CONNECT_CASE(N) case N: active = detail::set_of_indices(connect_at(static_cast<const Automaton<N>&>(a), active_alphabet_size, prov, finisher)); break;
 		TOGGLESRUNNER_CONNECT_CASE(4)
 		TOGGLESRUNNER_CONNECT_CASE(5)
 		TOGGLESRUNNER_CONNECT_CASE(6)
@@ -242,9 +246,12 @@ AutomatonBase::SymbolSet connect_deleted_symbols(const AutomatonBase& a, unsigne
 			std::terminate();
 	}
 
-	AutomatonBase::SymbolSet active = a.activeAlphabet(), deleted;
-	if (active_alphabet_size < 2) return deleted; //can't happen?
-	//We should now be dense for 0..alpha-2.
+	if (active.size() > active_alphabet_size-2)
+		//This would have caught a wrong-reports bug, so it's not just an assert.
+		throw std::logic_error(fmt::format("can't happen: connect didn't change locations? {} {}",
+				active.size(), active_alphabet_size));
+	AutomatonBase::SymbolSet deleted;
+	//We expect to be be dense for 0..alpha-2; anything missing was deleted.
 	for (unsigned int i = 0; i < active_alphabet_size-2; ++i)
 		if (!active.count(i))
 			deleted.insert(i);