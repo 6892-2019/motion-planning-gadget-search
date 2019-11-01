08e1fa83518462a70478fe7e48ada04dc87cf45a
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2019-11-01 02:56:46 -0400

    Delete unused files

--- src/pinning.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef PINNING_HPP
-#define PINNING_HPP
-
-void pin_this_thread(int core_number);
-
-#endif /* PINNING_HPP */
-
--- src/worker.cpp
+++
@@ -1,9 +0,0 @@
-#include "precompiled.hpp"
-#include "worker.hpp"
-#include "pinning.hpp"
-
-void worker_thread(int core_number, bounded_queue<std::function<void()>>& queue) {
-	pin_this_thread(core_number);
-	while (true)
-		queue.take()();
-}
