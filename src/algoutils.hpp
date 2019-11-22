1cae42cb6af107217dad4940451f2cbfb30948bc
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2019-11-22 17:06:38 -0500

    merge_unique: avoid *i++
    While technically required for input iterators, pop_front_iterator can't
    efficiently support it.  As it's a notational convenience only, and
    removed in future C++ standards, let's avoid it here.
    
    We have similar changes to make for the various interval_* functions.

--- src/algoutils.hpp
+++ src/algoutils.hpp
@@ -264,12 +264,15 @@ struct null_output_iterator {
 template<typename InputIter1, typename InputIter2, typename OutputIter>
 OutputIter merge_unique(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter output) {
 	while (first1 != last1 && first2 != last2) {
-		if (*first1 < *first2)
-			*output++ = *first1++;
-		else if (*first2 < *first1)
-			*output++ = *first2++;
-		else {
-			*output++ = *first1++;
+		if (*first1 < *first2) {
+			*output++ = *first1;
+			first1++;
+		} else if (*first2 < *first1) {
+			*output++ = *first2;
+			first2++;
+		} else {
+			*output++ = *first1;
+			first1++;
 			first2++;
 		}
 	}
