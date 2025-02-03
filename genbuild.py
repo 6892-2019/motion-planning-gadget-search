57e8c9ccf6b359b6a2e380095d4e5b11aaca4e7b
Jeffrey Bosboom <jbosboom@jeffreybosboom.com>
2025-02-02 18:33:30 -0800

    Use the default linker rather than gold
    As of binutils 2.44 (February 2025), gold is deprecated due to lack of
    maintenance.

--- genbuild.py
+++ genbuild.py
@@ -22,7 +22,7 @@ global_flags = OrderedDict()
 global_flags['builddir'] = 'build' # significant to Ninja
 global_flags['modeflags'] = '-std=c++2a'
 global_flags['warnflags'] = '-pedantic -Wall -Wextra -Wuninitialized -Winit-self -Wconversion -Wuseless-cast -Wlogical-op -Waggressive-loop-optimizations -Winvalid-pch -Wno-unused-parameter -Wduplicated-cond -Wnull-dereference -Wno-dangling-else -Wsuggest-override -fdiagnostics-color=always'
-global_flags['ldflags'] = '-fuse-ld=gold -Wl,--gc-sections -Wl,--gdb-index -u malloc -ljemalloc_pic -lc -lpthread -ldl -lfmt'
+global_flags['ldflags'] = '-Wl,--gc-sections -u malloc -ljemalloc_pic -lc -lpthread -ldl -lfmt'
 
 # configuration-specific flag *templates*
 config_flags = OrderedDict()
